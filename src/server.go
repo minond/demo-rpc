da57db50a48a875c8bb5f4fecdab423b962ec100
Marcos Minond <minond.marcos@gmail.com>
2018-03-28 17:33:58 -0600

    rpc server for friends service

--- src/server.go
+++ src/server.go
@@ -1,16 +1,45 @@
 package main
 
 import (
-	pb "github.com/minond/demo-rpc/friends"
+	"errors"
+	"log"
+	"net"
+
+	pb "github.com/minond/demo-rpc/src/friends"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
-const port = ":50007"
+const (
+	port = ":50007"
+)
 
-var db = make(map[string]pb.Friend)
+var (
+	db = make(map[string]pb.Friend)
+)
 
 type server struct{}
 
-// func (s *server)
+func (s *server) Search(*pb.SearchRequest, pb.Friends_SearchServer) error {
+	return errors.New("Unimplemented method")
+}
+
+func (s *server) Create(pb.Friends_CreateServer) error {
+	return errors.New("Unimplemented method")
+}
 
 func main() {
+	conn, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatalf("Failed to open tcp connection: %v", err)
+	}
+
+	serv := grpc.NewServer()
+	pb.RegisterFriendsServer(serv, &server{})
+	reflection.Register(serv)
+
+	if err := serv.Serve(conn); err != nil {
+		log.Fatalf("Failed to mount RPC service to connection: %v", err)
+	}
 }
